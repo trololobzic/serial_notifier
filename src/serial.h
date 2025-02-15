a6099952ec533b74f239cc71244fee7def5905dc
Egor Ukhov <uhov87@mail.ru>
2025-02-15 21:47:30 +0200

    refactoring

--- src/serial.h
+++ src/serial.h
@@ -27,7 +27,7 @@ struct SerialDevice
         return this_idx < other_idx;
     }
 
-protected:
+private:
     UINT32 _get_device_idx() const
     {
         CString copy_of_device_name = device_name;
@@ -46,8 +46,39 @@ public:
     typedef std::vector<SerialDevice> SerialList;
 
 
+    Serial() :
+        _serials_reg_section(HKEY_LOCAL_MACHINE),
+        _serials_reg_path(TEXT("HARDWARE\\DEVICEMAP\\SERIALCOMM"))
+    {
+        //TODO
+    }
+
+    SerialList read_serial_list()
+    {
+        SerialList serial_list;
+        TRegistry::EntriesSet reg_entries;
+        if (!TRegistry::get_key_value(_serials_reg_section, _serials_reg_path, reg_entries))
+        {
+            return serial_list;
+        }
+
+        for(TRegistry::EntriesSet::iterator it = reg_entries.begin(); it != reg_entries.end(); it++)
+        {
+            SerialDevice serial_device;
+            serial_device.friendly_name = it->key();
+            serial_device.device_name = it->get<CString>();
+
+            serial_list.push_back(serial_device);
+        }
+
+        std::sort(serial_list.begin(), serial_list.end());
+        return serial_list;
+    }
+
 private:
     SerialList _serial_list;
+    const HKEY _serials_reg_section;
+    const CString _serials_reg_path;
 };
 
 }
--- tests/serial_tests.cpp
+++ tests/serial_tests.cpp
@@ -4,30 +4,32 @@
 #define CATCH_CONFIG_NO_CPP11
 #include "catch.hpp"
 
-TEST_CASE("Checking SerialDevice->_get_device_idx()", "[serial]")
+/*
+* Test helpers
+*/
+struct TestRegistrySerialList1
 {
-    struct TestSerialDevice : public serial_notifier::SerialDevice
+    typedef std::vector<serial_notifier::RegistryEntry> EntriesSet;
+    inline static bool get_key_value(const HKEY section, const TCHAR * path, EntriesSet & result)
     {
-        UINT32 test_get_device_idx() const
-        {
-            return _get_device_idx();
-        }
-    };
+        (void)section;
+        (void)path;
 
-    TestSerialDevice serial_device;
+        result.resize(5);
+        result.at(0) = serial_notifier::RegistryEntry(CString(TEXT("/dev/ttyS9")), CString(TEXT("COM9")));
+        result.at(1) = serial_notifier::RegistryEntry(CString(TEXT("/dev/ttyS2")), CString(TEXT("COM2")));
+        result.at(2) = serial_notifier::RegistryEntry(CString(TEXT("/dev/ttyS10")), CString(TEXT("COM10")));
+        result.at(3) = serial_notifier::RegistryEntry(CString(TEXT("/dev/ttyS1")), CString(TEXT("COM1")));
+        result.at(4) = serial_notifier::RegistryEntry(CString(TEXT("/dev/ttyS12")), CString(TEXT("COM12")));
 
-    serial_device.device_name = TEXT("COM1");
-    REQUIRE(serial_device.test_get_device_idx() == 1);
+        return true;
+    }
+};
 
-    serial_device.device_name = TEXT("COM53");
-    REQUIRE(serial_device.test_get_device_idx() == 53);
 
-    serial_device.device_name = TEXT("_SMTH67A");
-    REQUIRE(serial_device.test_get_device_idx() == 67);
-
-    serial_device.device_name = TEXT("ABRACADABRA");
-    REQUIRE(serial_device.test_get_device_idx() == 0);
-}
+/*
+* Test cases
+*/
 
 TEST_CASE("Checking SerialDevice compare operators", "[serial]")
 {
@@ -46,4 +48,50 @@ TEST_CASE("Checking SerialDevice compare operators", "[serial]")
     REQUIRE_FALSE(device1 == device2);
     REQUIRE(device1 != device2);
     REQUIRE(device1 < device2);
+}
+
+TEST_CASE("Checking SerialDevice _get_device_idx()", "[serial]")
+{
+    std::vector<serial_notifier::SerialDevice> vec;
+    vec.resize(7);
+    vec.at(0).device_name = TEXT("ABRACADABRA");
+    vec.at(1).device_name = TEXT("COM10");
+    vec.at(2).device_name = TEXT("COM1_");
+    vec.at(3).device_name = TEXT("COM12");
+    vec.at(4).device_name = TEXT("COM11");
+    vec.at(5).device_name = TEXT("COM6");
+    vec.at(6).device_name = TEXT("COM2");
+
+    std::sort(vec.begin(), vec.end());
+
+    REQUIRE(::lstrcmp(vec.at(0).device_name, TEXT("ABRACADABRA")) == 0);
+    REQUIRE(::lstrcmp(vec.at(1).device_name, TEXT("COM1_")) == 0);
+    REQUIRE(::lstrcmp(vec.at(2).device_name, TEXT("COM2")) == 0);
+    REQUIRE(::lstrcmp(vec.at(3).device_name, TEXT("COM6")) == 0);
+    REQUIRE(::lstrcmp(vec.at(4).device_name, TEXT("COM10")) == 0);
+    REQUIRE(::lstrcmp(vec.at(5).device_name, TEXT("COM11")) == 0);
+    REQUIRE(::lstrcmp(vec.at(6).device_name, TEXT("COM12")) == 0);
+}
+
+TEST_CASE("Checking read_serial_list()", "[serial]")
+{
+    serial_notifier::Serial<TestRegistrySerialList1> serial;
+    serial_notifier::Serial<TestRegistrySerialList1>::SerialList serial_list = serial.read_serial_list();
+
+    REQUIRE(serial_list.size() == 5);
+
+    REQUIRE(::lstrcmp(serial_list.at(0).device_name, TEXT("COM1")) == 0);
+    REQUIRE(::lstrcmp(serial_list.at(0).friendly_name, TEXT("/dev/ttyS1")) == 0);
+
+    REQUIRE(::lstrcmp(serial_list.at(1).device_name, TEXT("COM2")) == 0);
+    REQUIRE(::lstrcmp(serial_list.at(1).friendly_name, TEXT("/dev/ttyS2")) == 0);
+
+    REQUIRE(::lstrcmp(serial_list.at(2).device_name, TEXT("COM9")) == 0);
+    REQUIRE(::lstrcmp(serial_list.at(2).friendly_name, TEXT("/dev/ttyS9")) == 0);
+
+    REQUIRE(::lstrcmp(serial_list.at(3).device_name, TEXT("COM10")) == 0);
+    REQUIRE(::lstrcmp(serial_list.at(3).friendly_name, TEXT("/dev/ttyS10")) == 0);
+
+    REQUIRE(::lstrcmp(serial_list.at(4).device_name, TEXT("COM12")) == 0);
+    REQUIRE(::lstrcmp(serial_list.at(4).friendly_name, TEXT("/dev/ttyS12")) == 0);
 }
